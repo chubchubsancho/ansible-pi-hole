f66dfd56609514559ee0bb3a1c213e9400820d9f
Grégory SANCHEZ <gregory.sanchez@icloud.com>
2022-05-04 10:28:20 +0200

    fix(molecule): add `pytest` and start testinfra test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_prerequisites_packages(host):
+    p = host.package('git')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert p.is_installed
